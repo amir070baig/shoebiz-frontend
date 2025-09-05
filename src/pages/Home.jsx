bbf0b6d82d75467f040f9859919a1662bff787bc
AmirTechDev <amirbaig070@gmail.com>
2025-09-05 23:58:41 +0530

    added gitignore

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const res = await axios.get("http://localhost:5000/api/products");
+        const res = await axios.get(`${import.meta.env.VITE_API_URL}/products`);
         setProducts(res.data);
       } catch (error) {
         console.error("Error fetching products:", error);
